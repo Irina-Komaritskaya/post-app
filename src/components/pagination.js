415a073722207f13cbea79ac3869e5d8fdad9df4
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2023-06-09 22:27:14 +0200

    pagination

---
+++ src/components/pagination.js
@@ -0,0 +1,37 @@
+import Pagination from "react-bootstrap/Pagination";
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+export const Paging = ({ items }) => {
+    const [active, setActive] = useState(1);
+    const [itemsPage, setItemsPage] = useState([]);
+
+    useEffect(() => {
+        const slicedItems = items.slice(
+            (active - 1) * 10,
+            (active - 1) * 10 + 10
+        );
+
+        setItemsPage(slicedItems);
+    }, [active, items]);
+
+    let pages = [];
+    for (let number = 1; number <= items.length / 10; number++) {
+        pages.push(
+            <Pagination.Item
+                key={number}
+                active={number === active}
+                onClick={() => {
+                    setActive(number);
+                }}
+            >
+                {number}
+            </Pagination.Item>
+        );
+    }
+    return (
+        <Container>
+            {itemsPage}
+            <Pagination>{pages}</Pagination>
+        </Container>
+    );
+};
--- src/components/posts.js
+++ src/components/posts.js
@@ -5,44 +5,22 @@ import { getPosts } from "../store/actions";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Post } from "./post";
-import Pagination from "react-bootstrap/Pagination";
 import Container from "react-bootstrap/Container";
 import { Search } from "./search";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { Paging } from "./pagination";
 export const Posts = () => {
     const { posts } = useSelector((state) => state.posts);
     const { filteredPosts } = useSelector((state) => state.posts);
     const dispatch = useDispatch();
-    const [active, setActive] = useState(1);
-    const [postsForPage, setPostForPage] = useState([]);
+
     console.log(filteredPosts);
+
     useEffect(() => {
         dispatch(getPosts());
     }, []);
 
-    useEffect(() => {
-        const slicedPosts = posts.slice(
-            (active - 1) * 10,
-            (active - 1) * 10 + 10
-        );
-        setPostForPage(slicedPosts);
-    }, [active, posts]);
-
-    let pages = [];
-    for (let number = 1; number <= posts.length / 10; number++) {
-        pages.push(
-            <Pagination.Item
-                key={number}
-                active={number === active}
-                onClick={() => {
-                    setActive(number);
-                }}
-            >
-                {number}
-            </Pagination.Item>
-        );
-    }
     return (
         <>
             <Search data={posts} />
@@ -52,12 +30,11 @@ export const Posts = () => {
                 <Button variant="secondary">3</Button>
                 <Button variant="secondary">4</Button>
             </ButtonGroup>
-            <Container>
-                {postsForPage.map((x) => (
+            <Paging
+                items={posts.map((x) => (
                     <Post data={x} />
                 ))}
-                <Pagination>{pages}</Pagination>
-            </Container>
+            ></Paging>
         </>
     );
 };
