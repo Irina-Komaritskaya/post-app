c6634eb76c90ed4f4d9a80ce2ec65416175a9e7b
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2023-06-12 23:47:15 +0200

    gh-page hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,7 +11,7 @@ import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router base="/">
             <Provider store={store}>
                 <App />
             </Provider>
